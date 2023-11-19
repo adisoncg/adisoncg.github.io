4af8359c04ef4bd644a761484dda639d44ca24d4
Adison <adison-cg@hotmail.com>
2023-11-19 02:00:37 -0300

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/adisoncg/gerenciador_financeiro"""
    This reverts commit cf1aeacab892733a53adff3a8cf205077dca80d3.

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d5p!8oe8fu^m+bxg)+tfdqtmj8f(q$(=coo!l^9^y%@zasxdy!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,10 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
