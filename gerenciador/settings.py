30d717836416efdc08ce49336a32a136d63f7df5
Adison <adison-cg@hotmail.com>
2023-11-20 19:23:55 -0300

    mudanças nas settings 2

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'gerenciador/staticfile/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfile'),
+    (BASE_DIR, 'staticfile'),
 ]
 
 
