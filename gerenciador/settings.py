bc04168a8408ca11183a76e35f792ff9aa30894d
Adison <adison-cg@hotmail.com>
2023-11-20 19:41:50 -0300

    modificação static

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,10 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'gerenciador/static/'
-
 STATICFILES_DIRS = [
-
+        os.path.join(BASE_DIR, 'static')
     ]
 
 
