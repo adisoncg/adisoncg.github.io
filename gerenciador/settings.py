51654bd958eb1048fa2474fb927bacae9ecdb40e
Adison <adison-cg@hotmail.com>
2023-11-20 20:39:22 -0300

    static usuarios

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -130,14 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# STATIC_ROOT = BASE_DIR / 'usuarios/staticfile/'
-
-# STATICFILES_DIRS = [
-   
-# ]
-
+STATIC_URL = '/static/usuarios/'
 
 
 # Default primary key field type
