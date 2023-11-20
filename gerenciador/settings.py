dd9fd58871fc2318ef3063088e28ab75f04402bb
Adison <adison-cg@hotmail.com>
2023-11-19 21:03:04 -0300

    modificando para vercel

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d5p!8oe8fu^m+bxg)+tfdqtmj8f(q$(=coo!l^9^y%@zasxdy!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
