66cd862fac36baa9c2244e390ba6efec006a46c9
Adison <adison-cg@hotmail.com>
2023-11-20 19:21:02 -0300

    mudanças nas settings

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 ]
 
 ROOT_URLCONF = 'gerenciador.urls'
@@ -132,10 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'usuarios/staticfile/'
+STATIC_ROOT = BASE_DIR / 'gerenciador/staticfile/'
 
 STATICFILES_DIRS = [
-   
+    os.path.join(BASE_DIR, 'staticfile'),
 ]
 
 
