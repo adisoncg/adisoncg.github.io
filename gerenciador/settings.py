f78f23b547cf673fde9a6daac37bd7447f96ea3e
Adison <adison-cg@hotmail.com>
2023-11-20 20:17:32 -0300

    Revert "mudanças nas settings 4"
    This reverts commit d98380ad68bf48505e6c375ac11ba5a01778734b.

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'gerenciador/static/'
+STATIC_ROOT = BASE_DIR / 'gerenciador/staticfile/'
 
 STATICFILES_DIRS = [
-
+    
     ]
 
 
